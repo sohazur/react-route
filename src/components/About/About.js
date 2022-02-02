7745aa9c6287f830a75654717bbfd82dce2417a9
Sohazur <mdsohazurislam@gmail.com>
2022-02-02 18:22:48 +0400

    Added components

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is about us page</h2>
+      <p>You can know anything about us.</p>
+    </div>
+  );
+};
+
+export default About;
