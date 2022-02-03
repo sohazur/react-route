ee47b380dc8dc14080d3a6ae948a99917e74e3ac
Sohazur <mdsohazurislam@gmail.com>
2022-02-03 05:23:34 +0400

    Component for single posts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Friends from "./components/Friends/Friends";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
+import PostDetail from "./components/PostDetail/PostDetail";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/friend/:friendId" element={<FriendDetail />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/about/culture" element={<Culture />} />
+        <Route path="/posts/:postId" element={<PostDetail />} />
       </Routes>
     </div>
   );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Post = (props) => {
-  const { title } = props.post;
+  const { title, id } = props.post;
   return (
     <div>
       <h2> {title} </h2>
+      <Link to={`/posts/${id}`}>Post Detail</Link>
     </div>
   );
 };
